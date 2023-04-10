32802add231b0d82013f41ec1fefde99955ced9f
jasondaiki <jasongementiza0@gmail.com>
2023-04-10 10:10:59 +0800

    Added some buttons

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const playerSide = document.getElementById("player")
+const playerX = document.getElementById("x")
+const playerO = document.getElementById("o")
 
+playerSide.addEventListener("click", () => {
+    const playerModal = document.getElementById("playermodal")
+    playerModal.style.visibility = "visible"
+})
+
+playerX.addEventListener("click", () => {
+    const playerModal = document.getElementById("playermodal")
+    playerModal.style.visibility = "hidden"
+})
+playerO.addEventListener("click", () => {
+    const playerModal = document.getElementById("playermodal")
+    playerModal.style.visibility = "hidden"
+})
